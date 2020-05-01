08c95bf460bc2179205b12b4f698276b626a1339
cyberspacedk <frontend.dk@gmail.com>
2020-05-01 17:43:36 +0300

    create pages

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const About = () => {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+      <p>
+        Some text
+      </p>
+    </Layout>
+  )
+}
+
+export default About;
---
+++ src/pages/company/team.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Layout from '../../components/layout'
+
+const Team = () => {
+  return (
+    <Layout>
+      <h1>Our Team</h1>
+      <p>
+        Some text
+      </p>
+    </Layout>
+  )
+}
+
+export default Team;
