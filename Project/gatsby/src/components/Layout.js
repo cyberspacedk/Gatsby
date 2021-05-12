dad53ca02ecad97525b6183a60bacf4a91c970c1
cyberspacedk <frontend.dk@gmail.com>
2021-05-12 10:37:59 +0300

    add Global styles

--- Project/gatsby/src/components/Layout.js
+++ Project/gatsby/src/components/Layout.js
@@ -1,12 +1,17 @@
 import React from 'react';
+
 import Footer from './Footer';
 import Nav from './Nav';
+
+import GlobalStyles from '../styles/GlobalStyles';
+import 'normalize.css';
 
 const Layout = ({ children }) => (
   <div>
     <Nav />
     {children}
     <Footer />
+    <GlobalStyles />
   </div>
 );
 
